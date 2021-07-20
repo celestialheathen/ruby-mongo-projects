b3f218b8040c124561a5afc892ec84bba1b04aad
celestialheathen <jingchaoyu@hotmail.com>
2021-07-20 17:05:41 -0400

    connection

--- connection_db.rb
+++ connection_db.rb
@@ -4,7 +4,6 @@ client = Mongo::Client.new('mongodb://127.0.0.1:27017/schoolDB')
 db = client.database
 
 begin  
-  p db.collections # Collection objects
   p db.collection_names # Collection names
 rescue 
   puts "Error"
